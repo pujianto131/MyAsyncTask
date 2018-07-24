1e0f29766313e213532927124c0f007e9119ab60
pujianto131 <pujianto.e131@gmail.com>
2018-07-24 22:40:47 +0700

    final

--- app/src/main/java/com/pujianto131/myasynctask/MainActivity.java
+++ app/src/main/java/com/pujianto131/myasynctask/MainActivity.java
@@ -1,13 +1,44 @@
 package com.pujianto131.myasynctask;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String DEMO_ASYNC = "DemoAsync";
+    private TextView tvStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvStatus = (TextView)findViewById(R.id.tv_status);
+        DemoAsync demoAsync = new DemoAsync();
+        demoAsync.execute("Hallo ini Demo Async");
+    }
+    private class DemoAsync extends AsyncTask<String, Void, String>{
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+            tvStatus.setText("status: onPreExecute");
+        }
+        @Override
+        protected String doInBackground(String... params){
+            Log.d(DEMO_ASYNC, "status: doInBackground");
+            try {
+                Thread.sleep(5000);
+            }
+            catch (Exception e) {
+                Log.d(DEMO_ASYNC, e.getMessage());
+            }
+            return params[0];
+        }
+        @Override
+        protected void onPostExecute(String s){
+            super.onPostExecute(s);
+            tvStatus.setText("status : onPostExecute : "+s);
+        }
     }
 }
